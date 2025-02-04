e120e8046295ee8b9e4de0b254aacf4aadf35052
Marius <108870491+mariusraupach@users.noreply.github.com>
2025-02-04 19:19:46 +0100

    update lua/plugins init.lua lazy-lock.json (#2)

--- init.lua
+++ init.lua
@@ -1,16 +1,20 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.opt.clipboard      = "unnamedplus"
+vim.opt.expandtab      = true
+vim.g.mapleader        = " "
+vim.opt.number         = true
+vim.opt.relativenumber = true
+vim.opt.shiftwidth     = 2
+vim.opt.softtabstop    = 2
+vim.opt.tabstop        = 2
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath         = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -20,4 +24,3 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
-
